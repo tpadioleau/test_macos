e128fd26f34755645dbff9c14a9c7db735dce246
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-02-24 09:21:25 +0100

    Add a plugin

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <Kokkos_Core.hpp>
 #include <paraconf.h>
 #include <pdi.h>
+#include <string>
 
-int main(int argc, char** argv)
-{
-    Kokkos::ScopeGuard scope(argc, argv);
-    PC_tree_t conf = PC_parse_string("");
-    PDI_init(conf);
-    PDI_finalize();
-    PC_tree_destroy(&conf);
-    return 0;
+int main(int argc, char **argv) {
+  std::string const pdi_conf(R"df(
+metadata:
+    a: int
+plugins:
+    trace:
+)df");
+  Kokkos::ScopeGuard const scope(argc, argv);
+  PC_tree_t conf = PC_parse_string(pdi_conf.c_str());
+  PDI_init(conf);
+  int a = 1;
+  PDI_expose("expose a", &a, PDI_INOUT);
+  PDI_finalize();
+  PC_tree_destroy(&conf);
+  return 0;
 }
